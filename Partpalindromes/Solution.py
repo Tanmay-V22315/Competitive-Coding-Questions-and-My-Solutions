e9c086aedda28de4beac2eb3173af550acc34aa8
Tanmay-V22315 <78300100+Tanmay-V22315@users.noreply.github.com>
2021-06-04 23:25:35 +0530

    Update Solution.py

--- Partpalindromes/Solution.py
+++ Partpalindromes/Solution.py
@@ -1,18 +1,16 @@
 def solution(inpstring):
-    inpstring = inpstring.lower()
-    inpstring = inpstring.replace(" ",'')
-    print(inpstring)
+    inpstring = inpstring.lower().replace(" ",'')
     letlist = list(inpstring)
     finlist = []
     palinset = []
-    for i in range(0,len(inpstring)+1):
-        for j in range(0,len(inpstring)+1):
+    for i in range(0,len(inpstring)+1):      #Generate all possible substrings of given string
+        for j in range(0,len(inpstring)+1):  
             if inpstring[i:i+j]!='':
                 finlist.append(inpstring[i:i+j])
     finlist.sort()
-    for i in finlist:
+    for i in finlist: #Check palindromes
         if list(i)==list(reversed(i)):
             palinset.append(i)
-    palinset.sort()
-    print(list(set(palinset)))
-solution(input("Word: "))
+    palinset.sort() #Not particularly important
+    return(list(set(palinset)))
+
