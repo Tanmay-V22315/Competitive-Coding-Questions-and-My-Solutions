8d492c2105d4e677ca604b93c6b5b25c1cdee235
Tanmay-V22315 <78300100+Tanmay-V22315@users.noreply.github.com>
2021-06-04 17:29:15 +0530

    Update Solution.py

--- 1044. Lucky Tickets./Solution.py
+++ 1044. Lucky Tickets./Solution.py
@@ -1,11 +1,18 @@
+import time
 #Brute-Force; will come up with a better solution. Anything Above 6 will take very long.
+#6 digits takes 1.3 seconds in python3
 def getlucky(N):        # Haha get it? 'Get Lucky' by Daft Punk and others.....you know what? Nevermind.
+    begin = time.time()
     delim = ''
     lastnum = delim.join([str(9)]*N)
     counter = 0
     for i in range(0,int(lastnum)+1):
         i = list(str(i).rjust(N,str(0)))
         i = [int(j) for j in i]
-        if sum(i[:len(i)//2])==sum(i[len(i)//2:len(i)]):
+        if sum(i[:int(len(i)*0.5)])==sum(i[int(len(i)*0.5):len(i)]):
             counter += 1
+    end = time.time()
+    print("Time taken by brute force: "+str(end-begin))
     return counter
+
+
