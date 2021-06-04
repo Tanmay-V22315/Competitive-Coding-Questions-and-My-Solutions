d7350be39dcc0e12d310e644ba2f73d336877ac2
Tanmay-V22315 <78300100+Tanmay-V22315@users.noreply.github.com>
2021-06-04 12:26:05 +0530

    Update Solution.py

--- Fibonacci-DP/Solution.py
+++ Fibonacci-DP/Solution.py
@@ -5,7 +5,6 @@ def solution(x):
     except:
         pass
     def fib(n): #Using Memoization DP; takes much less time.
-        begin = time.time()
         if n==1 or n==0:
             return n
         if memo[n]!=None:
@@ -13,10 +12,10 @@ def solution(x):
         else:
             memo[n]=fib(n-1)+fib(n-2)
         return(memo[n])
-        end = time.time()
     def fibdac(n): #Using Divide and Conquer; takes longer for larger numbers.
         if n==1 or n==0:
             return n
         else:
             return fibdac(n-2)+fibdac(n-1)
     return fib(x)
+print(solution(5))
