e51fc87a1accd884d8235edaf7ec78efde96a8c9
Tanmay-V22315 <78300100+Tanmay-V22315@users.noreply.github.com>
2021-06-05 12:38:48 +0530

    Create Solution.py

---
+++ Pan-digital Primes/Solution.py
@@ -0,0 +1,13 @@
+def solution(inpnum):
+    upnum = int(str(9)*inpnum)
+    lownum = int(str(1)+(str(0)*(inpnum-1)))
+    ansnum = None
+    for i in range(upnum,lownum,-1):
+        if len(str(i))==len(set(str(i))):
+            if 2**(i-1)%i==1:
+                ansnum = i
+                break
+
+
+    return ansnum
+print(solution(7))
