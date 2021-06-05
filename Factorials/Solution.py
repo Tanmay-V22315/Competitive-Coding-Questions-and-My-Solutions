437fb3fbc6e97cf14ae5d08c6aa083795ac1d770
Tanmay-V22315 <78300100+Tanmay-V22315@users.noreply.github.com>
2021-06-05 12:41:41 +0530

    Create Solution.py

---
+++ Factorials/Solution.py
@@ -0,0 +1,16 @@
+def solution(intinp):
+    global memo
+    memo = [None]*(intinp+1)
+    return getfact(intinp)
+def getfact(numinp):
+    if numinp==0:
+        return 1
+    elif numinp==1:
+        return 1
+    elif memo[numinp]!=None:
+        return memo[numinp]
+    else:
+        outsol = numinp*getfact(numinp-1)
+        memo[numinp-2]=outsol
+        return outsol
+print solution(45)
