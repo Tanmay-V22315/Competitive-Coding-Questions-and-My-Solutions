4305cfcd2ddfb715cae23ea013154293515db4ac
Tanmay-V22315 <78300100+Tanmay-V22315@users.noreply.github.com>
2021-07-02 20:47:21 +0530

    Optimization
    Checking only for even numbers and multiplying that by 2

--- 1044. Lucky Tickets./Solution.cpp
+++ 1044. Lucky Tickets./Solution.cpp
@@ -1,9 +1,10 @@
 //C++ solution, for dat speed!
-// Can still use some optimization but this is just for learning the language.
 #include <iostream>
 #include <cmath>
 #include <string> 
 using namespace std;
+
+
 int sumchecker(int internum, unsigned char numberOfDigits)
 {
     int sum = 0;
@@ -14,21 +15,27 @@ int sumchecker(int internum, unsigned char numberOfDigits)
     }
     return sum;
 }
+
+//------------------------------------------------------------------------------------------------------------------------------------------------------------------//
+
+
 int main()
 {   
     int x;
     cout << "Number of digits: " ;
     cin >> x;
     int counter = 0;
-    for (int y = 0; y < pow(10, x); y++)
-    {
-    
+    for (int y = 0; y < pow(10, x); y= y+2)
+    {   
         
         if (sumchecker(y%(int)pow(10, (x>>1)), x)==sumchecker(y - y%(int)pow(10, (x>>1)), x))
         {
             counter++;
+        
         }
+    
     }
-    cout <<  counter << endl;
+    cout <<  counter*2 << endl;
     return 0;
 }
+//4816030
